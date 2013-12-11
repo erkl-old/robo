19ab80522e628b529936f5a39074cc3baaed9307
Erik Lundin <erklundin@gmail.com>
2013-12-11 17:53:25 +0000

    Rearrange pattern_test.go

--- pattern_test.go
+++ pattern_test.go
@@ -4,40 +4,6 @@ import (
 	"testing"
 )
 
-var segmentTests = []struct {
-	segment segment
-	input   string
-	n       int
-}{
-	{segment{0, "/foo", nil}, "/foo", 4},
-	{segment{0, "/foo", nil}, "/bar", 0},
-	{segment{0, "/foo", nil}, "/foo/bar", 4},
-	{segment{0, "/foo/", nil}, "/foo/bar", 5},
-
-	{segment{1, "", nil}, "foo", 3},
-	{segment{1, "", nil}, "foo/bar", 3},
-	{segment{1, "", nil}, "/bar", 0},
-
-	{segment{2, "", []rune{'a', 'z'}}, "foo", 3},
-	{segment{2, "", []rune{'a', 'z'}}, "foo/", 3},
-	{segment{2, "", []rune{'a', 'z'}}, "/foo", 0},
-	{segment{2, "", []rune{'a', 'z', '0', '9'}}, "1a23foo", 7},
-
-	{segment{3, "", nil}, "foo", 3},
-	{segment{3, "", nil}, "foo/bar", 7},
-	{segment{3, "", nil}, "/a/b/c//", 8},
-}
-
-func TestSegmentMatch(t *testing.T) {
-	for _, test := range segmentTests {
-		if n := test.segment.match(test.input); n != test.n {
-			t.Errorf("%+v.match(%q)", test.segment, test.input)
-			t.Errorf("  got  %d", n)
-			t.Errorf("  want %d", test.n)
-		}
-	}
-}
-
 var patternTests = []struct {
 	pattern pattern
 	input   string
@@ -71,7 +37,41 @@ func TestPatternMatch(t *testing.T) {
 
 	fail:
 		t.Errorf("%+v.match(%q):", test.pattern, test.input)
-		t.Errorf("  want %v, %+v", test.ok, test.params)
+		t.Errorf("  got  %v, %+v", test.ok, test.params)
 		t.Errorf("  want %v, %+v", ok, params)
+	}
+}
+
+var segmentTests = []struct {
+	segment segment
+	input   string
+	n       int
+}{
+	{segment{0, "/foo", nil}, "/foo", 4},
+	{segment{0, "/foo", nil}, "/bar", 0},
+	{segment{0, "/foo", nil}, "/foo/bar", 4},
+	{segment{0, "/foo/", nil}, "/foo/bar", 5},
+
+	{segment{1, "", nil}, "foo", 3},
+	{segment{1, "", nil}, "foo/bar", 3},
+	{segment{1, "", nil}, "/bar", 0},
+
+	{segment{2, "", []rune{'a', 'z'}}, "foo", 3},
+	{segment{2, "", []rune{'a', 'z'}}, "foo/", 3},
+	{segment{2, "", []rune{'a', 'z'}}, "/foo", 0},
+	{segment{2, "", []rune{'a', 'z', '0', '9'}}, "1a23foo", 7},
+
+	{segment{3, "", nil}, "foo", 3},
+	{segment{3, "", nil}, "foo/bar", 7},
+	{segment{3, "", nil}, "/a/b/c//", 8},
+}
+
+func TestSegmentMatch(t *testing.T) {
+	for _, test := range segmentTests {
+		if n := test.segment.match(test.input); n != test.n {
+			t.Errorf("%+v.match(%q)", test.segment, test.input)
+			t.Errorf("  got  %d", n)
+			t.Errorf("  want %d", test.n)
+		}
 	}
 }
